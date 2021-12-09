28046ad6a1dc6d563e701159299c5609aa6613ca
Kamui131 <95751760+Kamui131@users.noreply.github.com>
2021-12-09 21:47:07 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const circle = document.querySelector('.circle');
-const square = document.querySelector('.square');
-circle.addEventListener("click", function() {(circle.style.backgroundColor = "blue");});
-square.addEventListener("click", function() {(square.style.backgroundColor = "yellow");});
